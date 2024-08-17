5b4a98b3be381f1e399049db7d1f4c0225f6989d
andrebettencourtt <andreluispb10@gmail.com>
2024-08-17 13:44:42 -0300

    feat: creating login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./global.css";
 
-createRoot(document.getElementById('root')!).render(
+import { LoginPage } from "./components/login.tsx";
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+    <LoginPage />
+  </StrictMode>
+);
