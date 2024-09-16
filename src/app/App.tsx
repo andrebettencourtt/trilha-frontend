6cba784a2a048cd0335bddaad09644acf9d61a50
andrebettencourtt <andreluispb10@gmail.com>
2024-09-16 20:39:52 -0300

    feat: main screen created

---
+++ src/app/App.tsx
@@ -0,0 +1,10 @@
+import "../css/global.css";
+import ConfigRouter from "../router/router";
+
+export function App() {
+  return (
+    <>
+      <ConfigRouter />
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./global.css";
 
-import { LoginPage } from "./components/login.tsx";
+import "./css/global.css";
+import { App } from "./app/App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <LoginPage />
   </StrictMode>
 );
