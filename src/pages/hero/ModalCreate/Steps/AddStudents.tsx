2747502fab7f798ab9bd2a2fff4f0dadc13f01a5
andrebettencourtt <andreluispb10@gmail.com>
2024-09-24 23:34:26 -0300

    fix: refactoring code

--- src/pages/hero/ModalCreate/Steps/AddStudents.tsx
+++ src/pages/hero/ModalCreate/Steps/AddStudents.tsx
@@ -1,4 +1,4 @@
-import { Plus, X } from "lucide-react";
+import { X } from "lucide-react";
 import styles from "./AddStudents.module.css";
 
 const AddStudents = () => {
@@ -28,10 +28,12 @@ const AddStudents = () => {
         </div>
         <div className={styles.lineModal} />
         <div className={styles.divButtonAddStudents}>
-          <button type="button" className={styles.buttonAddStudents}>
-            <p className={styles.textButtonStudents}>Adicionar alunos</p>{" "}
-            <Plus />
-          </button>
+          <select className={styles.buttonAddStudents}>
+            <option className={styles.textButtonStudents}>
+              Adicionar alunos
+            </option>{" "}
+            {/* <Plus /> */}
+          </select>
         </div>
       </div>
     </div>
